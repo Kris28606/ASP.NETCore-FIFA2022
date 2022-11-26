33c71793c99386b950427e3d9b2d96f71270e45c
Kris28606 <stanisavljevickristina28@gmail.com>
2022-11-26 16:27:51 +0100

    Add project files.

---
+++ Domain/FifaContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Domain
+{
+    public class FifaContext : DbContext
+    {
+        public FifaContext([NotNull]DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Team> Teams { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb; Database=FIFA2022; Trusted_Connection=True;");
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<Team>();
+
+        }
+    }
+}
---
+++ Domain/Team.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class Team
+    {
+        public int TeamId { get; set; }
+        public string Name { get; set; }
+        public string Continent { get; set; }
+        public int GoalDifference { get; set; }
+        public int Points { get; set; }
+    }
+}
